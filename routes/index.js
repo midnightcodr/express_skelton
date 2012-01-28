7ca3e3b02da2f5bdff91ac8f953213971fbcfd6e
Rico Chen <xiakingc@hotmail.com>
2012-01-28 11:46:51 -0500

    init commit

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' });
+};
+exports.test = function(req, res){
+  res.render('test', { title: 'Playground', jquery:true, jqueryui:true })
+};
